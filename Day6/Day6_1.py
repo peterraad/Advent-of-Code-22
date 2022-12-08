15ab22d8116dbb85d9028f5d5a06f424fd2884a0
Peter Raad <praaab@gmail.com>
2022-12-07 20:51:17 -0500

    day 6 p 1

---
+++ Day6/Day6_1.py
@@ -0,0 +1,19 @@
+with open("signal.txt") as rpsFile:
+    lines = []
+    
+    start_packet_position = 0
+    end_packet_position = 4
+    
+    for line in rpsFile:
+        line = line.rstrip()
+        
+        potential_signal = line[start_packet_position:end_packet_position]
+
+        while len(set(potential_signal)) != len(potential_signal):
+            start_packet_position +=1
+            end_packet_position +=1
+
+            potential_signal = line[start_packet_position:end_packet_position]
+
+        print(end_packet_position)
+
